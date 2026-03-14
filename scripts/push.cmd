89519cda7f88462ae16c5944d8e6f672b0686749
staale <staale.stokkereit@gmail.com>
2026-03-14 16:11:37 +0100

    update

--- scripts/push.cmd
+++ scripts/push.cmd
@@ -1,10 +1,13 @@
 @echo off
 
+REM Move from scripts folder to repo root
+cd /d "%~dp0.."
+
 echo Pulling latest changes from GitHub...
 git pull origin main --no-edit
 
 echo Adding files...
-git add .
+git add -A
 
 echo Committing...
 git commit -m "update"
